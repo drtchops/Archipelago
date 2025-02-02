512844e0622fdd7b475c14fe1e8168a8a9496fa6
Ian Robinson <ianronrobinson@gmail.com>
2025-02-01 19:03:45 -0500

    shuffle things around

--- worlds/toem/options.py
+++ worlds/toem/options.py
@@ -1,8 +1,14 @@
 from dataclasses import dataclass
 
-from Options import PerGameCommonOptions, StartInventoryPool
+from Options import PerGameCommonOptions, StartInventoryPool, Toggle
+
+
+class RandomizeThing(Toggle):
+    display_name = "Randomize Thing"
+    default = 1
 
 
 @dataclass
 class ToemOptions(PerGameCommonOptions):
     start_inventory_from_pool: StartInventoryPool
+    randomize_thing: RandomizeThing
