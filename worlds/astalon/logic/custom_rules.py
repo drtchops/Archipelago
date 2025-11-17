599b22438b7a1ee9003543f453736943f297126a
Ian Robinson <ianronrobinson@gmail.com>
2025-11-16 21:09:57 -0500

    refactor all the rules

--- worlds/astalon/logic/custom_rules.py
+++ worlds/astalon/logic/custom_rules.py
@@ -1,7 +1,7 @@
 import dataclasses
 from collections.abc import Iterable
 from enum import Enum
-from typing import TYPE_CHECKING, Any, ClassVar
+from typing import TYPE_CHECKING, Any, ClassVar, cast
 
 from typing_extensions import override
 
@@ -13,7 +13,6 @@ from Options import Option
 from ..constants import GAME_NAME
 from ..items import (
     BlueDoor,
-    Character,
     Crystal,
     Elevator,
     Events,
@@ -22,7 +21,6 @@ from ..items import (
     ItemName,
     KeyItem,
     RedDoor,
-    ShopUpgrade,
     Switch,
     WhiteDoor,
 )
@@ -31,7 +29,6 @@ from ..options import (
     Difficulty,
     Goal,
     RandomizeBlueKeys,
-    RandomizeCharacters,
     RandomizeElevator,
     RandomizeRedKeys,
     RandomizeSwitches,
@@ -43,40 +40,9 @@ if TYPE_CHECKING:
     from ..world import AstalonWorld
 
 
-ITEM_DEPS: dict[str, tuple[str, ...]] = {
-    KeyItem.CLOAK.value: (Character.ALGUS.value,),
-    KeyItem.SWORD.value: (Character.ARIAS.value,),
-    KeyItem.BOOTS.value: (Character.ARIAS.value,),
-    KeyItem.CLAW.value: (Character.KYULI.value,),
-    KeyItem.BOW.value: (Character.KYULI.value,),
-    KeyItem.BLOCK.value: (Character.ZEEK.value,),
-    KeyItem.STAR.value: (Character.BRAM.value,),
-    KeyItem.BANISH.value: (Character.ALGUS.value, Character.ZEEK.value),
-    KeyItem.GAUNTLET.value: (Character.ARIAS.value, Character.BRAM.value),
-    ShopUpgrade.ALGUS_ARCANIST.value: (Character.ALGUS.value,),
-    ShopUpgrade.ALGUS_METEOR.value: (Character.ALGUS.value,),
-    ShopUpgrade.ALGUS_SHOCK.value: (Character.ALGUS.value,),
-    ShopUpgrade.ARIAS_GORGONSLAYER.value: (Character.ARIAS.value,),
-    ShopUpgrade.ARIAS_LAST_STAND.value: (Character.ARIAS.value,),
-    ShopUpgrade.ARIAS_LIONHEART.value: (Character.ARIAS.value,),
-    ShopUpgrade.KYULI_ASSASSIN.value: (Character.KYULI.value,),
-    ShopUpgrade.KYULI_BULLSEYE.value: (Character.KYULI.value,),
-    ShopUpgrade.KYULI_RAY.value: (Character.KYULI.value,),
-    ShopUpgrade.ZEEK_JUNKYARD.value: (Character.ZEEK.value,),
-    ShopUpgrade.ZEEK_ORBS.value: (Character.ZEEK.value,),
-    ShopUpgrade.ZEEK_LOOT.value: (Character.ZEEK.value,),
-    ShopUpgrade.BRAM_AXE.value: (Character.BRAM.value,),
-    ShopUpgrade.BRAM_HUNTER.value: (Character.BRAM.value,),
-    ShopUpgrade.BRAM_WHIPLASH.value: (Character.BRAM.value,),
-}
-
-VANILLA_CHARACTERS: frozenset[str] = frozenset((Character.ALGUS.value, Character.ARIAS.value, Character.KYULI.value))
-
-characters_off = [rule_builder.OptionFilter(RandomizeCharacters, RandomizeCharacters.option_vanilla)]
-characters_on = [rule_builder.OptionFilter(RandomizeCharacters, RandomizeCharacters.option_vanilla, operator="gt")]
-
-
-def as_str(value: Enum | str) -> str:
+def as_str(value: Enum | str | None) -> str:
+    if value is None:
+        return ""
     return value.value if isinstance(value, Enum) else value
 
 
@@ -85,54 +51,12 @@ class Has(rule_builder.Has["AstalonWorld"], game=GAME_NAME):
     @override
     def __init__(
         self,
-        item_name: ItemName | Events | str,
+        item_name: ItemName | Events,
         count: int = 1,
         *,
         options: Iterable[rule_builder.OptionFilter[Any]] = (),
     ) -> None:
         super().__init__(as_str(item_name), count, options=options)
-
-    @override
-    def _instantiate(self, world: "AstalonWorld") -> rule_builder.Rule.Resolved:
-        default = self.Resolved(
-            self.item_name,
-            self.count,
-            player=world.player,
-            caching_enabled=world.rule_caching_enabled,
-        )
-
-        if self.item_name in VANILLA_CHARACTERS:
-            if world.options.randomize_characters.value == RandomizeCharacters.option_vanilla:
-                return world.true_rule
-            return default
-
-        if deps := ITEM_DEPS.get(self.item_name):
-            if world.options.randomize_characters.value == RandomizeCharacters.option_vanilla and (
-                len(deps) > 1 or (len(deps) == 1 and deps[0] in VANILLA_CHARACTERS)
-            ):
-                return default
-            if len(deps) == 1:
-                return HasAll.Resolved(
-                    (deps[0], self.item_name),
-                    player=world.player,
-                    caching_enabled=world.rule_caching_enabled,
-                )
-            return rule_builder.Or.Resolved(
-                tuple(
-                    world.get_cached_rule(
-                        HasAll.Resolved(
-                            (d, self.item_name),
-                            player=world.player,
-                            caching_enabled=world.rule_caching_enabled,
-                        )
-                    )
-                    for d in deps
-                ),
-                player=world.player,
-                caching_enabled=world.rule_caching_enabled,
-            )
-
-        return default
 
 
 @dataclasses.dataclass(init=False)
@@ -140,7 +64,7 @@ class HasAll(rule_builder.HasAll["AstalonWorld"], game=GAME_NAME):
     @override
     def __init__(
         self,
-        *item_names: ItemName | Events | str,
+        *item_names: ItemName | Events,
         options: Iterable[rule_builder.OptionFilter[Any]] = (),
     ) -> None:
         names = [as_str(name) for name in item_names]
@@ -148,80 +72,6 @@ class HasAll(rule_builder.HasAll["AstalonWorld"], game=GAME_NAME):
             raise ValueError(f"Duplicate items detected, likely typo, items: {names}")
 
         super().__init__(*names, options=options)
-
-    @override
-    def _instantiate(self, world: "AstalonWorld") -> rule_builder.Rule.Resolved:
-        if len(self.item_names) == 0:
-            return world.true_rule
-        if len(self.item_names) == 1:
-            return Has(self.item_names[0]).resolve(world)
-
-        new_clauses: list[rule_builder.Rule.Resolved] = []
-        new_items: list[str] = []
-        for item in self.item_names:
-            if (
-                item in VANILLA_CHARACTERS
-                and world.options.randomize_characters.value == RandomizeCharacters.option_vanilla
-            ):
-                continue
-            deps = ITEM_DEPS.get(item, [])
-            if not deps:
-                new_items.append(item)
-                continue
-
-            if len(deps) > 1:
-                if world.options.randomize_characters.value == RandomizeCharacters.option_vanilla:
-                    new_items.append(item)
-                else:
-                    new_clauses.append(
-                        rule_builder.Or.Resolved(
-                            tuple(
-                                world.get_cached_rule(
-                                    HasAll.Resolved(
-                                        (d, item),
-                                        player=world.player,
-                                        caching_enabled=world.rule_caching_enabled,
-                                    )
-                                )
-                                for d in deps
-                            ),
-                            player=world.player,
-                            caching_enabled=world.rule_caching_enabled,
-                        )
-                    )
-                continue
-
-            if (
-                len(deps) == 1
-                and deps[0] not in self.item_names
-                and not (
-                    deps[0] in VANILLA_CHARACTERS
-                    and world.options.randomize_characters.value == RandomizeCharacters.option_vanilla
-                )
-            ):
-                new_items.append(deps[0])
-
-            new_items.append(item)
-
-        if len(new_clauses) == 0 and len(new_items) == 0:
-            return world.true_rule
-        if len(new_items) == 1:
-            new_clauses.append(
-                Has.Resolved(new_items[0], player=world.player, caching_enabled=world.rule_caching_enabled)
-            )
-        elif len(new_items) > 1:
-            new_clauses.append(
-                HasAll.Resolved(tuple(new_items), player=world.player, caching_enabled=world.rule_caching_enabled)
-            )
-        if len(new_clauses) == 0:
-            return rule_builder.False_.Resolved(player=world.player, caching_enabled=world.rule_caching_enabled)
-        if len(new_clauses) == 1:
-            return new_clauses[0]
-        return rule_builder.And.Resolved(
-            tuple(world.get_cached_rule(c) for c in new_clauses),
-            player=world.player,
-            caching_enabled=world.rule_caching_enabled,
-        )
 
 
 @dataclasses.dataclass(init=False)
@@ -229,7 +79,7 @@ class HasAny(rule_builder.HasAny["AstalonWorld"], game=GAME_NAME):
     @override
     def __init__(
         self,
-        *item_names: ItemName | Events | str,
+        *item_names: ItemName | Events,
         options: Iterable[rule_builder.OptionFilter[Any]] = (),
     ) -> None:
         names = [as_str(name) for name in item_names]
@@ -238,90 +88,14 @@ class HasAny(rule_builder.HasAny["AstalonWorld"], game=GAME_NAME):
 
         super().__init__(*names, options=options)
 
-    @override
-    def _instantiate(self, world: "AstalonWorld") -> rule_builder.Rule.Resolved:
-        if len(self.item_names) == 0:
-            return world.true_rule
-        if len(self.item_names) == 1:
-            return Has(self.item_names[0]).resolve(world)
-
-        new_clauses: list[rule_builder.Rule.Resolved] = []
-        new_items: list[str] = []
-        for item in self.item_names:
-            if (
-                item in VANILLA_CHARACTERS
-                and world.options.randomize_characters.value == RandomizeCharacters.option_vanilla
-            ):
-                return world.true_rule
-
-            deps = ITEM_DEPS.get(item, [])
-            if not deps:
-                new_items.append(item)
-                continue
-
-            if len(deps) > 1:
-                if world.options.randomize_characters.value == RandomizeCharacters.option_vanilla:
-                    new_items.append(item)
-                else:
-                    new_clauses.append(
-                        rule_builder.Or.Resolved(
-                            tuple(
-                                world.get_cached_rule(
-                                    HasAll.Resolved(
-                                        (d, item),
-                                        player=world.player,
-                                        caching_enabled=world.rule_caching_enabled,
-                                    )
-                                )
-                                for d in deps
-                            ),
-                            player=world.player,
-                            caching_enabled=world.rule_caching_enabled,
-                        )
-                    )
-                continue
-
-            if (
-                len(deps) == 1
-                and deps[0] not in self.item_names
-                and not (
-                    deps[0] in VANILLA_CHARACTERS
-                    and world.options.randomize_characters.value == RandomizeCharacters.option_vanilla
-                )
-            ):
-                new_clauses.append(
-                    HasAll.Resolved((deps[0], item), player=world.player, caching_enabled=world.rule_caching_enabled)
-                )
-            else:
-                new_items.append(item)
-
-        if len(new_items) == 1:
-            new_clauses.append(
-                Has.Resolved(new_items[0], player=world.player, caching_enabled=world.rule_caching_enabled)
-            )
-        elif len(new_items) > 1:
-            new_clauses.append(
-                HasAny.Resolved(tuple(new_items), player=world.player, caching_enabled=world.rule_caching_enabled)
-            )
-
-        if len(new_clauses) == 0:
-            return rule_builder.False_.Resolved(player=world.player, caching_enabled=world.rule_caching_enabled)
-        if len(new_clauses) == 1:
-            return new_clauses[0]
-        return rule_builder.Or.Resolved(
-            tuple(world.get_cached_rule(c) for c in new_clauses),
-            player=world.player,
-            caching_enabled=world.rule_caching_enabled,
-        )
-
 
 @dataclasses.dataclass(init=False)
 class CanReachLocation(rule_builder.CanReachLocation["AstalonWorld"], game=GAME_NAME):
     @override
     def __init__(
         self,
-        location_name: LocationName | str,
-        parent_region_name: RegionName | str = "",
+        location_name: LocationName,
+        parent_region_name: RegionName | None = None,
         skip_indirect_connection: bool = False,
         *,
         options: Iterable[rule_builder.OptionFilter[Any]] = (),
@@ -334,7 +108,7 @@ class CanReachRegion(rule_builder.CanReachRegion["AstalonWorld"], game=GAME_NAME
     @override
     def __init__(
         self,
-        region_name: RegionName | str,
+        region_name: RegionName,
         *,
         options: Iterable[rule_builder.OptionFilter[Any]] = (),
     ) -> None:
@@ -346,8 +120,8 @@ class CanReachEntrance(rule_builder.CanReachEntrance["AstalonWorld"], game=GAME_
     @override
     def __init__(
         self,
-        from_region: RegionName | str,
-        to_region: RegionName | str,
+        from_region: RegionName,
+        to_region: RegionName,
         *,
         options: Iterable[rule_builder.OptionFilter[Any]] = (),
     ) -> None:
@@ -362,10 +136,11 @@ class ToggleRule(HasAll, game=GAME_NAME):
 
     @override
     def _instantiate(self, world: "AstalonWorld") -> rule_builder.Rule.Resolved:
-        if len(self.item_names) == 1:
-            rule = Has(self.item_names[0], options=[rule_builder.OptionFilter(self.option_cls, 1)])
+        items = tuple(cast(ItemName | Events, item) for item in self.item_names)
+        if len(items) == 1:
+            rule = Has(items[0], options=[rule_builder.OptionFilter(self.option_cls, 1)])
         else:
-            rule = HasAll(*self.item_names, options=[rule_builder.OptionFilter(self.option_cls, 1)])
+            rule = HasAll(*items, options=[rule_builder.OptionFilter(self.option_cls, 1)])
 
         if self.otherwise:
             return rule_builder.Or(
